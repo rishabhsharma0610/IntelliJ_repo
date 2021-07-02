2f839e9eda86518988a4ed9557309152bb0fd623
Rishabh Sharma <R@cavisson.com>
2021-07-02 14:24:32 +0530

    "HPD_JAVA"|cavisson

---
+++ cavisson/default/default/scripts/HPD_JAVA/exit_script.java
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------------------
+    Name: exit_script
+    Recorded By: cavisson
+    Date of recording: 07/02/2021 02:23:24
+    Flow details:
+    Build details: 4.6.1 (build# 32)
+    Modification History:
+-----------------------------------------------------------------------------*/
+
+package HPD_JAVA;
+import pacJnvmApi.NSApi;
+
+public class exit_script
+{
+    public static int execute(NSApi nsApi)
+    {
+        return 0;
+    }
+}
