2ef4040b4925b0f0c9412ffbc4efe3bf555b64d4
Rishabh Sharma <R@cavisson.com>
2021-07-06 14:48:59 +0530

    "newe_35"|cavisson

---
+++ cavisson/default/default/scripts/JAVA_35/init_script.java
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------------------
+    Name: init_script
+    Recorded By: cavisson
+    Date of recording: 07/06/2021 02:43:52
+    Flow details:
+    Build details: 4.6.1 (build# 35)
+    Modification History:
+-----------------------------------------------------------------------------*/
+
+package JAVA_35;
+import pacJnvmApi.NSApi;
+
+public class init_script
+{
+    public static int execute(NSApi nsApi)
+    {
+        return 0;
+    }
+}
